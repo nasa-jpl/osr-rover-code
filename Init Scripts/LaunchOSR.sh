799ce1bdbf870e36e5b83fdb2b392cfbf189d3cc
Achllle <achille.verheye@gmail.com>
2020-06-14 14:14:48 -0700

    Rename init scripts folder to be more linux friendly, make services work for me

--- Init Scripts/LaunchOSR.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-bash -c ". /home/pi/osr_ws/devel/setup.sh"
-bash -c ". /home/pi/osr_ws/devel/setup.bash"
-bash -c ". /opt/ros/kinetic/setup.sh"
-bash -c ". /opt/ros/kinetic/setup.bash"
-bash -i -c "roslaunch osr_bringup osr.launch"
---
+++ init_scripts/LaunchOSR.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /opt/ros/melodic/setup.bash
+source /home/achille/osr/osr_ws/devel/setup.bash
+roslaunch osr_bringup osr.launch &
