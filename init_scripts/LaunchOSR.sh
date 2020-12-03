7d60363f666c67def2299114bd83515d4b79c01a
Kit Kennedy <kitkennedy8@gmail.com>
2020-12-03 14:17:52 -0800

    Adding capability to use an osr_mod.launch file with bespoke changes for running on your system. Will default to the original file if that's not found

--- init_scripts/LaunchOSR.sh
+++ init_scripts/LaunchOSR.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+
+source osr_paths.sh
+launch_dir=$OSR_CODE_DIR/ROS/osr_bringup/launch
+
 bash -c ". /home/$USER/osr_ws/devel/setup.sh"
 bash -c ". /home/$USER/osr_ws/devel/setup.bash"
 bash -c ". /opt/ros/kinetic/setup.sh"
 bash -c ". /opt/ros/kinetic/setup.bash"
-bash -i -c "roslaunch osr_bringup osr.launch"
+
+# execute the custom mod launch file if it's available
+if [ -e "$launch_dir/osr_mod.launch" ]; then
+    bash -i -c "roslaunch osr_bringup osr_mod.launch"
+# otherwise go with the default
+else
+    bash -i -c "roslaunch osr_bringup osr.launch"
+fi
