e066c7fc12aac504cafc9ff4130d0c7e7886a3b4
Achllle <achille.verheye@gmail.com>
2021-04-12 05:03:24 +0000

    Elaborate on serial1 vs serial0, extend roboclawtest.py

--- scripts/roboclawtest.py
+++ scripts/roboclawtest.py
@@ -9,17 +9,23 @@ from time import sleep
 import sys
 from os import path
 # need to add the roboclaw.py file in the path
-sys.path.append(path.join(path.expanduser('~'), 'osr_ws/src/osr-rover-code/ROS/osr_control/osr_control'))
+sys.path.append(path.join(path.expanduser('~'), 'osr2_ws/src/osr-rover-code/ROS/osr_control/osr_control'))
 from roboclaw import Roboclaw
 
 if __name__ == "__main__":
     
     address = int(sys.argv[1]) 
-    roboclaw = Roboclaw("/dev/serial1", 115200)
-    try:
-        assert roboclaw.Open() == 1
-    except AssertionError as e:
-        raise e("Could not open comport /dev/serial1, make sure it has the correct permissions and is available")
-
-    print(roboclaw.ReadVersion(address))
-    print(roboclaw.ReadEncM1(address))
+    roboclaw0 = Roboclaw("/dev/serial0", 115200)
+    roboclaw1 = Roboclaw("/dev/serial1", 115200)
+    connected0 = roboclaw0.Open() == 1
+    connected1 = roboclaw1.Open() == 1
+    if connected0:
+        print("Connected to /dev/serial0.")
+        print(roboclaw0.ReadVersion(address))
+        print(roboclaw0.ReadEncM1(address))
+    elif connected1:
+        print("Connected to /dev/serial1.")
+        print(roboclaw1.ReadVersion(address))
+        print(roboclaw1.ReadEncM1(address))
+    else:
+        print("Could not open comport /dev/serial0 or /dev/serial1, make sure it has the correct permissions and is available")
