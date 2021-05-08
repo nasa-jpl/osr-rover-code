30efb75d26ea488c9ba9d38a5293814db8d42716
Achllle <achille.verheye@gmail.com>
2021-05-08 21:32:55 +0000

    Make service scripts user agnostic, remove LaunhOSR.sh (unused), move to /usr/local//bin

--- init_scripts/LaunchOSR.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-bash -c ". /opt/ros/foxy/setup.sh"
-bash -c ". /opt/ros/foxy/setup.bash"
-bash -c ". /home/$USER/osr_ws/install/setup.sh"
-bash -c ". /home/$USER/osr_ws/install/setup.bash"
-bash -i -c "ros2 launch osr_bringup osr_launch.py"
