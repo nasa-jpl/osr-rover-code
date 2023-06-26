3281013c93c54acdc6e14552298101f10267d005
Achllle <achille.verheye@gmail.com>
2023-06-26 05:34:00 +0000

    Add config for rcs

--- scripts/rc_config.py
+++ scripts/rc_config.py
@@ -9,11 +9,16 @@ if __name__ == "__main__":
     
     rc = test_connection(address)
 
+    # for RC1 (128): 0x80a3
     # rc.SetConfig(address, 32931)
+    # for RC2 (129): 0x81a3
+    # rc.SetConfig(address, 33187)
+    # for RC3 (130): 0x82a3
+    # rc.SetConfig(address, 33443)
 
     config = rc.GetConfig(address)[1]
     print(f"Config (int): {config}, (hex) {hex(config)}")
 
-    rc.WriteNVM(address)
+    # rc.WriteNVM(address)
 
     
