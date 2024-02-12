f3c0bc4775466dda4f2a24b4d5b68051b3fa731d
Achllle <achille.verheye@gmail.com>
2024-02-12 18:02:06 +0000

    Set default address for ina260 to 0x45

--- scripts/ina260/test_ina260.py
+++ scripts/ina260/test_ina260.py
@@ -4,10 +4,10 @@ from ina260.controller import Controller
 parser = argparse.ArgumentParser()
 addr_help = \
 """
-i2c address of your ina260 in hexadecimal. Default 0x40. You can verify that it is detected using 'sudo i2cdetect 1'.
+i2c address of your ina260 in hexadecimal. Default 0x45. You can verify that it is detected using 'sudo i2cdetect 1'.
 You should see one entry that's not empty in the table. Specify that number as the argument to this script, e.g. 0x41.
 """
-parser.add_argument("address", help=addr_help, type=lambda x: int(x, 16), nargs='?', default='0x40')
+parser.add_argument("address", help=addr_help, type=lambda x: int(x, 16), nargs='?', default='0x45')
 args = parser.parse_args()
 
 print(f"Attempting to connect INA260 at address {hex(args.address)}...\n")
