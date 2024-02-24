e41d16c65f1ff0193b958ccc4c1b2417061203ae
Achllle <achille.verheye@gmail.com>
2024-02-24 05:39:01 +0000

    Add a hand-tuned factor to allow duty control at full power

--- ROS/osr_control/osr_control/roboclaw_wrapper.py
+++ ROS/osr_control/osr_control/roboclaw_wrapper.py
@@ -37,6 +37,7 @@ class RoboclawWrapper(Node):
                 ('corner_acceleration_factor', Parameter.Type.DOUBLE),
                 ('velocity_timeout', Parameter.Type.DOUBLE),
                 ('duty_mode', Parameter.Type.BOOL),
+                ('velocity_qpps_to_duty_factor', Parameter.Type.INTEGER),
                 ('roboclaw_mapping.drive_left_front.address', Parameter.Type.INTEGER),
                 ('roboclaw_mapping.drive_left_front.channel', Parameter.Type.STRING),
                 ('roboclaw_mapping.drive_left_front.ticks_per_rev', Parameter.Type.INTEGER),
@@ -112,6 +113,7 @@ class RoboclawWrapper(Node):
         self.velocity_timeout = rclpy.duration.Duration(seconds=self.get_parameter('velocity_timeout').get_parameter_value().double_value, 
                                                         nanoseconds=0)
         self.duty_mode = self.get_parameter('duty_mode').get_parameter_value().bool_value
+        self.velocity_qpps_to_duty_factor = self.get_parameter('velocity_qpps_to_duty_factor').get_parameter_value().integer_value
         self.time_last_cmd = self.get_clock().now()
 
         self.stop_motors()
@@ -305,6 +307,8 @@ class RoboclawWrapper(Node):
         :param target_qpps: int
         """
         # clip values
+        if self.duty_mode:
+            target_qpps *= self.velocity_qpps_to_duty_factor
         target_qpps = max(-self.roboclaw_overflow, min(self.roboclaw_overflow, target_qpps))
         if channel == "M1":
             if self.duty_mode:
